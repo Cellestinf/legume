3739b3837a242e589e18c5b07dd67538628323f9
Cellestinf <kolyasnikov_vadim_1999@mail.ru>
2024-09-15 22:34:32 +0700

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,1 +1,9 @@
-
+immutable_var = (5, 1.5, 'text')
+print(immutable_var)
+# immutable_var[0] = 3
+# При попытке замены данных в кортеже, выпадает ошибка:
+# TypeError: 'tuple' object does not support item assignment
+# связанно это с тем что особенностью кортежа является неизменяемость данных.
+mutable_list = [1.2, 13, 'string']
+mutable_list[0] = int(mutable_list[0])
+print(mutable_list)
